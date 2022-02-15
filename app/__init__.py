8e3a5f9558a5afb2da57e224dc7f43bd307dcc02
stevesdawg <5642476+stevesdawg@users.noreply.github.com>
2022-02-15 11:16:41 -0500

    Change config object to a default config object.
    Add logic to load private config from config.json file in app/cfg directory.

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,16 @@
 from flask import Flask
-from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+import config
+
+
 app = Flask(__name__)
-app.config.from_object(Config)
+app.config.from_object(config.DefaultConfig)
+success = app.config.from_json("cfg/config.json", silent=True)
+if not success:
+    raise RuntimeError("no config.json file in app/cfg/ directory")
 db = SQLAlchemy(app)
 migrate = Migrate(app, db, compare_type=True)
 
-from app import routes, models, Votes, Budget
+from app import routes, models
